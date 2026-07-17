7d2c40c673582e5ad4fe80857757db09a62ff912
Sohail Ahmad <sohailwork2004@gmail.com>
2026-07-17 19:27:42 +0500

    Add JavaScript DataTypes Example

---
+++ Basics/02_DataTypes.js
@@ -0,0 +1,13 @@
+let name = "Sohail Ahmad";
+let age = 21;
+let isStudent = false;
+let city = null;
+let country;
+
+
+
+console.log(typeof name); // string
+console.log(typeof age); // number
+console.log(typeof isStudent); // boolean
+console.log(typeof city); // object (null is considered an object in JavaScript)
+console.log(typeof country); // undefined (not assigned a value yet)

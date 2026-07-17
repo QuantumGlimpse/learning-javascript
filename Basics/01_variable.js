bffe19110292ce8f43fce803355cbbade7efb0a9
Sohail Ahmad <sohailwork2004@gmail.com>
2026-07-17 16:59:57 +0500

    Add javascript variable example

--- Basics/01_variable.js
+++ Basics/01_variable.js
@@ -1,7 +1,12 @@
 let name = "John Doe";
 let age = 30;
+let isStudent = true;
+let country = "Pakistan";
 
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
+console.log("Country:", country);
 
-
-console.log("Hello, World!");
-console.error("This is an error message.");
+#console.log("Hello, World!");
+#console.error("This is an error message.");

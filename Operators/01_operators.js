d4993b61cde58668f0ba2aa93186e04a1a7789aa
Sohail Ahmad <sohailwork2004@gmail.com>
2026-07-18 15:41:21 +0500

    Add JavaScript Operators example

---
+++ Operators/01_operators.js
@@ -0,0 +1,23 @@
+let a = 5;
+let b = 10;
+
+// Addition
+let sum = a + b; // 15
+
+// Subtraction
+let difference = b - a; // 5
+
+// Multiplication
+let product = a * b; // 50
+
+// Division
+let quotient = b / a; // 2
+
+// Modulus
+let remainder = b % a; // 0
+
+console.log(`Sum: ${sum}`);
+console.log(`Difference: ${difference}`);
+console.log(`Product: ${product}`);
+console.log(`Quotient: ${quotient}`);
+console.log(`Remainder: ${remainder}`);

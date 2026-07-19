26028cd5e6fad5a0a7a0eeea31fc808509b8795e
Sohail Ahmad <sohailwork2004@gmail.com>
2026-07-19 16:37:02 +0500

    Add JavaScript Assignment Operators example

---
+++ Operators/02_assignment.js
@@ -0,0 +1,12 @@
+let a = 10;
+a += 5; // a = a + 5
+console.log(`Value of a after addition: ${a}`);
+a -= 3; // a = a - 3
+console.log(`Value of a after subtraction: ${a}`);
+a *= 2; // a = a * 2
+console.log(`Value of a after multiplication: ${a}`);
+a /= 4; // a = a / 4
+console.log(`Value of a after division: ${a}`);
+a %= 3; // a = a % 3
+
+console.log(`Final value of a: ${a}`);

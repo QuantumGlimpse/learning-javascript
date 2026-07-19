8541d14d481fcd8e584525add41f8d76527955d8
Sohail Ahmad <sohailwork2004@gmail.com>
2026-07-19 17:10:49 +0500

    Add JavaScript comparison operator example

---
+++ Operators/03_comparison.js
@@ -0,0 +1,12 @@
+let age = 20;
+
+console.log(age > 18); // true
+console.log(age < 18); // false
+console.log(age >= 20); // true
+console.log(age <= 20); // true
+
+
+console.log(age == "20"); // true
+console.log(age != "20"); // false
+console.log(age === 20); // true
+console.log(age !== "20"); // true

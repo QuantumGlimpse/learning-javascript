48e73b7dff6096a4ed012738bbc028c2e6c51c2d
Sohail Ahmad <sohailwork2004@gmail.com>
2026-08-02 05:37:57 +0500

    Add JavaScript for loop examples

---
+++ Operators/Loops/01_for_loop.js
@@ -0,0 +1,11 @@
+for(i=1 ; i<=10; i++ )  {
+  console.log("Increasing = "+i);
+}
+
+for(j=10 ; j>=1 ; j--)  {
+    console.log("Decreasing = "+j);
+}
+
+for(k=2 ; k<=10 ; k+=2) {
+    console.log("Evens = "+k)
+}

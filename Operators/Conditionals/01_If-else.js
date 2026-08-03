78657d62199204fc8203f4ecd572da188b63f0ca
Sohail Ahmad <sohailwork2004@gmail.com>
2026-08-03 12:44:00 +0500

    Add for Loop example

---
+++ Operators/Conditionals/01_If-else.js
@@ -0,0 +1,38 @@
+// Voting Eligibility Checker
+
+let age = 20;
+
+if (age  >= 18) {
+    console.log("You are eligible to vote.");
+} else {
+    console.log("You are not eligible to vote.");
+}
+
+
+// Grade Checker
+
+let marks = 85;
+
+if (marks >= 90) {
+    console.log("Grade: A");
+} else if (marks >= 80) {
+    console.log("Grade: B");
+} else if (marks >= 70) {
+    console.log("Grade: C");
+} else if (marks >= 60) {
+    console.log("Grade: D");
+} else {
+    console.log("Grade: F");
+}    
+
+//Positive, Negative, or Zero Checker
+
+let number = -5;   
+
+if (number > 0) {
+    console.log("The number is positive.");
+} else if (number < 0) {
+    console.log("The number is negative.");
+} else {
+    console.log("The number is zero.");
+}

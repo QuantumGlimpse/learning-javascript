44a7ea79e8c269f948858886c59118ccb27c59a9
Sohail Ahmad <sohailwork2004@gmail.com>
2026-08-03 12:30:06 +0500

    Add JavaScript Loop examples

--- Operators/Loops/01_for_loop.js
+++ Operators/Loops/01_for_loop.js
@@ -1,11 +1,44 @@
-for(i=1 ; i<=10; i++ )  {
-  console.log("Increasing = "+i);
+// For Loops
+
+for (let i = 1; i <= 10; i++) {
+  console.log("Increasing = " + i);
 }
 
-for(j=10 ; j>=1 ; j--)  {
-    console.log("Decreasing = "+j);
+for (let j = 10; j >= 1; j--) {
+  console.log("Decreasing = " + j);
 }
 
-for(k=2 ; k<=10 ; k+=2) {
-    console.log("Evens = "+k)
+for (let k = 2; k <= 10; k += 2) {
+  console.log("Evens = " + k);
+}
+
+
+// Do-While Loop
+
+let j = 0;
+
+do {
+  console.log("Multiples of 5 = " + j);
+  j += 5;
+} while (j <= 100);
+
+
+// While Loop
+
+let k = 1;
+
+while (k <= 50) {
+
+  if (k === 25) {
+    k += 2;        // Move to next odd number
+    continue;
+  }
+
+  console.log("Odd numbers = " + k);
+
+  if (k === 37) {
+    break;
+  }
+
+  k += 2;
 }
